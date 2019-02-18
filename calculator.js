34c194fad9d7cc8385e1fec199c611a04aa5e16d
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-02-18 15:28:23 +0800

    Add subtract method

--- calculator.js
+++ calculator.js
@@ -4,6 +4,16 @@ class Calculator {
   add(a, b) {
     return a + b;
   }
+
+  subtract(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+      throw new Error("Only numbers are allowed");
+    }
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("Only numbers are allowed");
+    }
+    return a - b;
+  }
 }
 
 module.exports = new Calculator();
--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,29 @@
 const calc = require("./calculator");
 
-test("adds two numbers correctly", () => {
-  const sum = calc.add(1, 2);
-  expect(sum).toBe(3);
+describe("Calculator", () => {
+  test("adds two numbers correctly", () => {
+    expect(calc.add(2, 5)).toBe(7);
+  });
+
+  test("subtracts 1 from 3 and returns 2", () => {
+    expect(calc.subtract(3, 1)).toBe(2);
+  });
+
+  test("subtracts 1 from 5 and returns 4", () => {
+    expect(calc.subtract(5, 1)).toBe(4);
+  });
+
+  test("throws when a is not a number", () => {
+    const error = new Error("Only numbers are allowed");
+    expect(() => {
+      calc.subtract("invalid", 1);
+    }).toThrow(error);
+  });
+
+  test("throws when b is not a number", () => {
+    const error = new Error("Only numbers are allowed");
+    expect(() => {
+      calc.subtract(1, NaN);
+    }).toThrow(error);
+  });
 });
