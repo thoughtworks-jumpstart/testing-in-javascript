4673ea51f7d07a7db4f437a3263dab78da122b64
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-02-18 10:28:27 +0800

    Use suggested lint

--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,6 @@
 const calc = require("./calculator");
 
-test('adds two numbers correctly', () => {
+test("adds two numbers correctly", () => {
   const sum = calc.add(1, 2);
   expect(sum).toBe(3);
-})
+});
